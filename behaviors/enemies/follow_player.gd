7d54e514539ecfe079adcd0afb5be2b60f360dab
danielfablicio <fabliciodaniel@gmail.com>
2024-08-07 09:39:37 -0300

    cut physics process to half of frames(60/s to 30/s)

--- behaviors/enemies/follow_player.gd
+++ behaviors/enemies/follow_player.gd
@@ -3,14 +3,19 @@ extends Node
 
 @onready var enemy: Enemy = get_parent()
 var distanceVector: Vector2
+var waitToActualize: bool = true
 
 func _physics_process(_delta):
 	if GameManager.playerDied || !GameManager.selectedPlayer: return
+	
+	waitToActualize = !waitToActualize
+	if waitToActualize: return
+	
 	if !enemy.doingAttack:
 		move()
 
 func move():
 	distanceVector = GameManager.playerPosition - enemy.position
-	var targetVelocity: Vector2 = distanceVector.normalized() * enemy.speed * 100
+	var targetVelocity: Vector2 = distanceVector.normalized() * enemy.speed * 100 * 2
 	enemy.velocity = enemy.velocity.lerp(targetVelocity, 0.4)
 	enemy.move_and_slide()
